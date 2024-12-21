53544e4e68f6cd0f73b9429623b6b717f74e90f4
Sourabh <sk25002kumar@gmail.com>
2024-12-21 14:26:31 +0530

    Add responsiveness

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/navbar_style.css";
 import logo from "../assets/poly_Icon.jpg";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
@@ -10,12 +16,15 @@ const Navbar = () => {
         <span className="navbar-title">Poly</span>
       </div>
       <div className="navbar-right">
-        <ul className="navbar-menu">
+        <button className="hamburger-menu" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`navbar-menu ${menuOpen ? "active" : ""}`}>
           <li>Product</li>
           <li>About</li>
           <li>Partners</li>
+          <button className="navbar-button">Get a Demo</button>
         </ul>
-        <button className="navbar-button">Get a Demo</button>
       </div>
     </nav>
   );
